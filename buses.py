9b4bd40c14bdb56f7394518b78d6d776db471717
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-12 14:25:15 +0200

    plantilla buses.py

--- buses.py
+++ buses.py
@@ -1,1 +1,9 @@
+from typing import TypeAlias
 
+BusesGraph: TypeAlias = networkx.Graph
+
+def get_buses_graph() -> BusesGraph:
+
+def show(g: BusesGraph) -> None:
+
+def plo(g: BusesGraph, nom_fitxer:str) -> None:
