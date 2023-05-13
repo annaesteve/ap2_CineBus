4eac472659cb4240753033421270bf2129853f87
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-13 15:57:30 +0200

    Update billboard.py

--- billboard.py
+++ billboard.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+import requests
+from bs4 import BeautifulSoup
+import urllib.request as ur
+import networkx
+import osmnx
+import haversine
+import staticmap
 
 @dataclass 
 class Film: 
@@ -29,4 +36,15 @@ class Billboard:
     cinemas: list[Cinema]
     projections: list[Projection]
 
-def read() -> Billboard: ...
+def read() -> Billboard:
+
+    for i in range(0,2):        #recorrem les tres pàgines de Sensacine d'on treuem la informació
+        #read and open url to scrape
+        urlToScrape = "https://https://www.sensacine.com/cines/cines-en-72480/ " + str(i * 10 + 1)"
+        r = ur.urlopen(urlToScrape).read()
+        soup = BeautifulSoup(r, "lxml")
+
+def search_by_name(name: str) -> list[Projection]:
+    """ Given a name, returns a list of the projections
+        that include this name in the title of the film"""
+    
