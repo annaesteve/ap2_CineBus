1511f1ca42de9500de8abc939cb69bf32118ba8f
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-17 16:40:54 +0200

    no llegeix cap film_ biçllboard.py

--- billboard.py
+++ billboard.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
 import urllib.request as ur
+import requests
 import json
 
 @dataclass
@@ -34,35 +35,37 @@ def read() -> Billboard:
     lprojections: list[Projection] = list()
 
     for i in range(3):  # Iterating through three pages of Sensacine
-        urlToScrape = "https://www.sensacine.com/cines/cines-en-72480/" + str(i * 10 + 1)
-        r = ur.urlopen(urlToScrape).read()
+        urlToScrape = "https://www.sensacine.com/cines/cines-en-72480/"
+        req = ur.Request(urlToScrape, headers={'User-Agent': 'Chrome/35.0.1916.47'})
+        r = ur.urlopen(req)
         soup = BeautifulSoup(r, "lxml")
-        
         moviesList = soup.find_all('div', class_="item_resa")
         
         for moviesListItem in moviesList:
             # Extract theater name
             try:
-                movieTheater = moviesListItem['data-theater']
-                theaterName = movieTheater['name']
-                cinema = Cinema(name = theaterName, address = '')
+                movieTheater = moviesListItem["data-theater"]
+                theaterName = movieTheater["name"]
+                cinema = Cinema(theaterName, '')
                 lcinemas.append(cinema)
             except KeyError:
-                theaterName = ''
+                theaterName = ""
             
             # Extract movie data
             try:
-                movieData = moviesListItem['data-movie']
-                movieTitle = movieData['title']
-                movieGenre = movieData['genre']
-                movieDirector = movieData['directors'][0]
-                movieActors = movieData['actors']
-                film_ = Film(title=movieTitle, genre=movieGenre, director=movieDirector, actors=movieActors)
+                movieData = moviesListItem["data-movie"]
+                movieTitle = movieData["title"]
+                movieGenre = movieData["genre"]
+                movieDirector = movieData["directors"][0]
+                movieActors = movieData["actors"]
+                film_ = Film(movieTitle, movieGenre, movieDirector, movieActors)
                 lfilms.append(film_)
             except KeyError:
-                movieTitle = ''
+                print("aqui")
+                movieTitle = ""
             
             # Extract data time
+            print(lfilms)
             try:
                 movieTime = json.loads(moviesListItem.find('em')['data-times'])
                 hour, minute = movieTime[0].split(':')
@@ -74,11 +77,10 @@ def read() -> Billboard:
             
             # Create objects and add to the projections list
            
-            
-            projection = Projection(film = film_, cinema = cinema, time =  time_tuple, language = '')
+            projection = Projection(film_, cinema, time_tuple, '')
             lprojections.append(projection)
 
-    return Billboard(films = lfilms, cinemas = lcinemas, projections = lprojections)
+    return Billboard(lfilms, lcinemas, lprojections)
 
 def search_by_name(name: str) -> list[Projection]:
     """ Given a name, returns a list of the projections
