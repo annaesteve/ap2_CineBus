78e6aa530e13442e91d84fbccb9ee108fba7192f
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-17 17:04:24 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -1,8 +1,29 @@
-from typing import TypeAlias
+from typing import TypeAlias, Tuple, Optional
+import networkx as nx
+from dataclasses import dataclass
+from bs4 import BeautifulSoup
+import urllib.request as ur
+import requests
+import json
 
-BusesGraph: TypeAlias = networkx.Graph
+BusesGraph: TypeAlias = nx.Graph()
+
+@dataclass
+class Node:
+    nom: str
+    coordenades: Tuple[int, int]
+
+def define_nodes() -> BusesGraph:
+    Parades: BusesGraph = ()
+    urlToScrape = "https://www.ambmobilitat.cat/OpenData/ObtenirDadesAMB.json"
+    req = requests.get(urlToScrape, headers={'User-Agent': 'Chrome/35.0.1916.47'})
+    r = ur.urlopen(req)
+    soup = BeautifulSoup(r, "lxml")
+    
+    return BusesGraph
 
 def get_buses_graph() -> BusesGraph:
+
 
 def show(g: BusesGraph) -> None:
 
