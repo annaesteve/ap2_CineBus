617e32bfc85a713daa6c8f58700b597c0d82d4da
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-18 09:45:46 +0200

    Update billboard.py

--- billboard.py
+++ billboard.py
@@ -34,55 +34,53 @@ def read() -> Billboard:
     lcinemas: list[Cinema] = list()
     lprojections: list[Projection] = list()
 
-    for i in range(3):  # Iterating through three pages of Sensacine
+    for _ in range(3):  # Iterating through three pages of Sensacine
         urlToScrape = "https://www.sensacine.com/cines/cines-en-72480/"
         req = ur.Request(urlToScrape, headers={'User-Agent': 'Chrome/35.0.1916.47'})
         r = ur.urlopen(req)
         soup = BeautifulSoup(r, "lxml")
         moviesList = soup.find_all('div', class_="item_resa")
         
-        for moviesListItem in moviesList:
-            # Extract theater name
-            try:
-                movieTheater = moviesListItem["data-theater"]
-                theaterName = movieTheater["name"]
-                cinema = Cinema(theaterName, '')
-                lcinemas.append(cinema)
-            except KeyError:
-                theaterName = ""
-            
-            # Extract movie data
-            try:
-                movieData = moviesListItem["data-movie"]
-                movieTitle = movieData["title"]
-                movieGenre = movieData["genre"]
-                movieDirector = movieData["directors"][0]
-                movieActors = movieData["actors"]
-                film_ = Film(movieTitle, movieGenre, movieDirector, movieActors)
-                lfilms.append(film_)
-            except KeyError:
-                print("aqui")
-                movieTitle = ""
-            
-            # Extract data time
-            print(lfilms)
-            try:
-                movieTime = json.loads(moviesListItem.find('em')['data-times'])
-                hour, minute = movieTime[0].split(':')
-                hour = int(hour)
-                minute = int(minute)
-                time_tuple = hour, minute
-            except (KeyError, json.JSONDecodeError, AttributeError):
-                time_tuple = 0,0
-            
-            # Create objects and add to the projections list
-           
-            projection = Projection(film_, cinema, time_tuple, '')
+        for movie in moviesList:
+            # Extract film information
+            film_data = movie.find('div', class_='j_w').get('data-movie')
+            film_info = json.loads(film_data)
+            title = film_info['title']
+            genre = film_info['genre']
+            directors = film_info['directors']
+            actors = film_info['actors']
+
+            # Extract cinema and time information
+            cinema_data = movie.find('div', class_='j_w').get('data-theater')
+            cinema_info = json.loads(cinema_data)
+            cinema_name = cinema_info['name']
+            cinema_address = cinema_info['city']
+
+            time_element = movie.find('ul', class_='list_hours').find('em')
+            times = json.loads(time_element.get('data-times'))
+
+            # Create Film object
+            film = Film(title, genre, directors, actors)
+
+            # Create Cinema object
+            cinema = Cinema(cinema_name, cinema_address)
+
+            # Create Projection object
+            projection = Projection(film, cinema, (times[0].split(':')[0], times[0].split(':')[1]), language='')
+
+            # Add objects to the respective lists
+            lfilms.append(film)
+            lcinemas.append(cinema)
             lprojections.append(projection)
 
-    return Billboard(lfilms, lcinemas, lprojections)
+    print(lprojections)
+    # Create Billboard object
+    billboard = Billboard(films=lfilms, cinemas=lcinemas, projections=lprojections)
+    
+    return billboard
 
-def search_by_name(name: str) -> list[Projection]:
+
+#def search_by_name(name: str) -> list[Projection]:
     """ Given a name, returns a list of the projections
         that include this name in the title of the film"""
 
