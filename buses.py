e88c65e13548d28a5a067361e525f7d1813b293e
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-19 20:41:22 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -14,19 +14,20 @@ class Stop:
     name: str
     line: list[str]
     coordinate: Tuple[float, float]
+    node: int
 
 @dataclass
 class Line:
     id: int
     stops: list[Stop]
 
-def create_stop(parada: dict[str, int|str]) -> Stop:
+def create_stop(parada: dict[str, int|str], num_node:int) -> Stop:
     node_nom = parada['Nom']
     node_linies: list[str] = list() 
     for l in parada['Linies'].split(' - '):
         node_linies.append(l)
     node_coordenades = (parada['UTM_X'], parada['UTM_Y'])
-    node = Stop(node_nom, node_linies, node_coordenades)
+    node = Stop(node_nom, node_linies, node_coordenades, num_node)
 
     return node
 
@@ -44,21 +45,28 @@ def define_nodes() -> BusesGraph:
         for line in lines:
             if line['Parades']['Parada'][0]['Municipi'] == 'Barcelona':
                 linia: Line = Line(0,[])
+                i = 0
                 for parada in line['Parades']['Parada']:
-                    node = create_stop(parada)
+                    node = create_stop(parada, num_node)
                     node_dict = asdict(node)
                     if len(node.line) == 1 or node.name not in llista_nodes_afegits:
                         Buses_graph.add_node(num_node, **node_dict)
                         llista_nodes_afegits.append(node.name)
                         num_node += 1
+                    
+                    if i > 0:
+                        Buses_graph.add_edge(linia.stops[i-1].node, num_node)
 
                     linia.stops.append(node)
+                    i += 1
                 linia.id = parada['IdLinia']
-
+    
+    Buses_graph.remove_node(num_node)
     return Buses_graph
 
 def show(g: BusesGraph) -> None:
-    nx.draw(g, with_labels=True, node_color='lightblue', edge_color='gray')
+    node_positions = nx.get_node_attributes(g, 'coordinate')
+    nx.draw(g, pos=node_positions, with_labels=True, node_color='lightblue', edge_color='gray')
     plt.show()
 
 def main() -> None:
@@ -70,6 +78,5 @@ if __name__ == '__main__':
     main()
 
 #def get_buses_graph() -> BusesGraph:
-
 
 #def plo(g: BusesGraph, nom_fitxer:str) -> None:
