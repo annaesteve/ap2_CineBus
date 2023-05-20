4daf5e0f43b152d890715600c1029198e3127feb
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-20 07:47:31 +0200

    Update city.py

--- city.py
+++ city.py
@@ -3,24 +3,45 @@ import networkx as nx
 from networkx import MultiDiGraph as MDG
 import osmnx as ox
 import matplotlib as plt 
+import pickle
+import os
+
 
 
 CityGraph : TypeAlias = nx.Graph()
+BusesGraph: TypeAlias = nx.Graph()
 OsmnxGraph: TypeAlias = MDG()
 Coord : TypeAlias = Tuple[float, float]   # (latitude, longitude)
 
-def get_osmnx_graph() -> None:
+def get_osmnx_graph() -> OsmnxGraph:
     graph = ox.graph_from_place("Barcelona, Spain", network_type='all')
-    ox.plot_graph(ox.project_graph(graph))
-    plt.show()
+    return graph
 
 
-#def save_osmnx_graph(g: OsmnxGraph, filename: str) -> None: ...
+def save_osmnx_graph(g: OsmnxGraph, file_name: str) -> None:
     # guarda el graf g al fitxer filename
-#def load_osmnx_graph(filename: str) -> OsmnxGraph: ...
+    #ox.plot_graph(ox.project_graph(g))
+    #plt.show()
+    with open(file_name, 'wb') as file:
+        pickle.dump(g, file)
+
+    if os.path.exists(file_name):
+        print("The file {} exists".format(file_name))
+    
+    else:
+        print("The file {} doesn't exist".format(file_name))
+
+
+def load_osmnx_graph(file_name: str) -> OsmnxGraph:
     # retorna el graf guardat al fitxer filename
 
-#def build_city_graph(g1: OsmnxGraph, g2: CityGraph) -> CityGraph: ...
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as file:
+            loaded_graph = pickle.load(file)
+
+    return loaded_graph
+
+def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph: ...
     # retorna un graf fusió de g1 i g2
 
 #def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: ...
@@ -33,7 +54,11 @@ def get_osmnx_graph() -> None:
     # mostra el camí p en l'arxiu filename
 
 def main() -> None:
-    get_osmnx_graph()
+    g = get_osmnx_graph()
+    save_osmnx_graph(g, 'graf_barcelona.pickle')
+    ox.plot_graph(ox.project_graph(load_osmnx_graph('graf_barcelona.pickle')))
+    plt.show()
+    
 
 if __name__ == '__main__':
     main()
