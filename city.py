dfb3a13367b0c68ac724c69697a6c82574c9b34e
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-21 17:02:36 +0200

    Update city.py

--- city.py
+++ city.py
@@ -5,11 +5,18 @@ import matplotlib as plt
 import pickle
 import os
 import buses
+from dataclasses import dataclass
 
 CityGraph : TypeAlias = nx.Graph()
 BusesGraph: TypeAlias = nx.Graph()
 OsmnxGraph: TypeAlias = nx.MultiDiGraph()
 Coord : TypeAlias = Tuple[float, float]   # (latitude, longitude)
+
+@dataclass
+class Path:
+    initial_position: Coord
+    final_position: Coord
+    distance: int
 
 def get_osmnx_graph() -> OsmnxGraph:
     graph = ox.graph_from_place("Barcelona, Spain", network_type='all')
@@ -46,20 +53,31 @@ def build_city_graph(g1: OsmnxGraph, g2: BusesGraph) -> CityGraph:
     ox.plot_graph(ox.project_graph(graf))
     plt.show()
 
-#def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path: ...
+def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
 
-#def show(g: CityGraph) -> None: ...
+    graph = nx.from_edgeslist(ox_g[['u', 'v', 'key']].values.tolist(), create_using=nx.MultiDiGraph)
+    start_node = ox.distance.nearest_nodes(g, src[0], src[1], method='haversine')
+    end_node = ox.distance.nearest_nodes(g, dst[0], dst[1], method='haversine')
+
+    path = nx.shortest_path(graph, source=start_node, target = end_node, weight = 'lenght')
+
+    return path
+
+def show(g: CityGraph) -> None:
     # mostra g de forma interactiva en una finestra
+    
+    ox.plot_graph(ox.project_graph(g))
+    plt.show()
+
 #def plot(g: CityGraph, filename: str) -> None: ...
     # desa g com una imatge amb el mapa de la cuitat de fons en l'arxiu filename
+
 #def plot_path(g: CityGraph, p: Path, filename: str, ...) -> None: ...
     # mostra el camí p en l'arxiu filename
 
 def main() -> None:
     g = get_osmnx_graph()
     save_osmnx_graph(g, 'graf_barcelona.pickle')
-    ox.plot_graph(ox.project_graph(load_osmnx_graph('graf_barcelona.pickle')))
-    plt.show()
     #build_city_graph(load_osmnx_graph('graf_barcelona.pickle'), buses.define_nodes())
     
 
