2f26e29a78d3ca25c9086f7b88b6ec19fe948871
annaesteve <77117385+annaesteve@users.noreply.github.com>
2023-05-31 02:10:53 +0200

    moltes llibreries i pocs llibres city.py

--- city.py
+++ city.py
@@ -9,6 +9,9 @@ import os
 import buses
 from dataclasses import dataclass
 from queue import Queue
+from shapely.ops import transform
+from functools import partial
+import pyproj
 
 
 CityGraph : TypeAlias = nx.Graph()
@@ -30,19 +33,37 @@ class Path:
 
 
 def get_osmnx_graph() -> OsmnxGraph:
-    graph = ox.graph_from_place("Barcelona, Spain", network_type='all', simplify=True)
-    
-    for u, v, key, geom in graph.edges(data = "geometry", keys = True):
-        if geom is not None:
-            del(graph[u][v][key]["geometry"])
 
+    epsg_code = 'EPSG:32631'
+
+    graph = ox.graph_from_place("Barcelona, Spain", network_type='all', truncate_by_edge=True)
+
+    graph_simplified = nx.Graph()
+    
+    graph_simplified.graph['crs'] = epsg_code
+
+    for node, data in graph.nodes(data=True):
+        x = data['x']
+        y = data['y']
+
+        new_node_data = {'x': x, 'y': y}
+
+        graph_simplified.add_node(node, **new_node_data)
+
+    attributes = ["length", "geometry"]
+
+    for u, v, data in graph.edges(data=True):
+        selected_attributes = {key: data[key] for key in attributes if key in data}
+        graph_simplified.add_edge(u, v, **selected_attributes )
+
+    build_city_graph(graph_simplified, buses.define_nodes())
     return graph
 
-def simplify_graph(g: OsmnxGraph) -> CityGraph:
+def simplify_graph_x(g: OsmnxGraph) -> nx.Graph:
     simple_g = nx.Graph()
     
     for node, data in g.nodes(data=True):
-        x, y = data['y'], data['x']
+        x, y = data['x'], data['y']
         simple_g.add_node(node, coordinate_x= x, coordinate_y = y)
 
     
@@ -50,9 +71,11 @@ def simplify_graph(g: OsmnxGraph) -> CityGraph:
         distance = data['length']
         simple_g.add_edge(u, v, distance= distance)
 
+    build_city_graph(simple_g, buses.define_nodes())
+
     return simple_g
 
-def save_osmnx_graph(g: CityGraph, file_name: str) -> None:
+def save_osmnx_graph(g: OsmnxGraph, file_name: str) -> None:
     """guarda el graf g al fitxer filename"""
 
     with open(file_name, 'wb') as file:
@@ -73,20 +96,40 @@ def load_osmnx_graph(file_name: str) -> CityGraph:
             return loaded_graph
     return None
 
-
-def build_city_graph(g1: CityGraph, g2: BusesGraph) -> CityGraph:
+def build_city_graph(g1: nx.Graph, g2: BusesGraph) -> CityGraph:
     # retorna un graf fusió de g1 i g2
-    graf = nx.compose(g1,g2)
+
+    g1.add_nodes_from(g2.nodes(data=True))
+    g1.add_edges_from(g2.edges(data=True))
 
 
     for node, atributtes in g2.nodes(data=True):
         nearest_node = ox.distance.nearest_nodes(g1, atributtes['coordinate'][0], atributtes['coordinate'][1])
-        graf.add_edge(node, nearest_node)
+        g1.add_edge(node, nearest_node)
+   
+    print('pas6')
 
-    nx.draw_networkx(graf)
+    #target_crs = pyproj.CRS('EPSG:32631')
+    #project = partial(
+    #    pyproj.transform,
+    #    pyproj.Proj(g1.graph['crs']),
+    #    target_crs
+    #)
+
+
+    #graph_proj = ox.project_graph(g1, to_crs=target_crs)
+    
+    #fig, ax = ox.plot_graph(graph_proj, show=False, close=False)
+
+    #ax.set_title('OSMnx Graph in EPSG:32631')
+
+    nx.draw(g1, with_labels=True, node_color='lightblue', edge_color='gray')
+
     plt.show()
 
-    return graf
+    print('pas7')
+
+    return g1
 
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
     graph = nx.from_edgeslist(ox_g[['u', 'v', 'key']].values.tolist(), create_using=nx.MultiDiGraph)
@@ -115,11 +158,11 @@ def show(g: CityGraph) -> None:
 
 def main() -> None:
     g = get_osmnx_graph()
-    s_g = simplify_graph(g)
-    #save_osmnx_graph(s_g, 'graf_barcelona.pickle')
-    print('si')
-    #show(load_osmnx_graph('graf_barcelona.pickle'))
-    show(s_g)
+    #s_g = simplify_graph(g)
+    #save_osmnx_graph(g, 'graf_barcelona.pickle')
+    #build_city_graph(load_osmnx_graph('graf_barcelona.pickle'), buses.define_nodes())
+
+    #show(s_g)
 
 if __name__ == '__main__':
     main()
