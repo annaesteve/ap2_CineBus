132799cd4ff9ce957ad5c3b68e1f6d732a37c6aa
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-06-03 12:12:47 +0200

    AMB EL PEP8 billboard.py

--- billboard.py
+++ billboard.py
@@ -5,6 +5,7 @@ import requests
 import json
 from datetime import date, datetime, time
 
+
 @dataclass
 class Film:
     title: str
@@ -12,10 +13,12 @@ class Film:
     director: str
     actors: list[str]
 
+
 @dataclass
 class Cinema:
     name: str
     address: str
+
 
 @dataclass
 class Projection:
@@ -24,22 +27,27 @@ class Projection:
     time: tuple[int, int]
     language: str
 
+
 @dataclass
 class Billboard:
     films: list[Film]
     cinemas: list[Cinema]
     projections: list[Projection]
 
+
 def read() -> Billboard:
+    """Returns today's Billboard, obtaining the data
+    from a web (web scraping)"""
     lfilms: list[Film] = list()
     lcinemas: list[Cinema] = list()
     lprojections: list[Projection] = list()
 
-    today = date.today()
-
-    for i in range(1,4):  # Iterating through three pages of Sensacine
-        urlToScrape = "https://www.sensacine.com/cines/cines-en-72480/?page=" + str(i)
-        req = ur.Request(urlToScrape, headers={'User-Agent': 'Chrome/35.0.1916.47'})
+    for i in range(1, 4):  # Iterating through three pages of Sensacine
+        urlToScrape = "https://www.sensacine.com/cines/cines-en-72480/?page=" + \
+            str(i)
+        req = ur.Request(
+            urlToScrape, headers={
+                'User-Agent': 'Chrome/35.0.1916.47'})
         r = ur.urlopen(req)
         soup = BeautifulSoup(r, "lxml")
         sList = soup.find_all('div', class_="tabs_box_pan item-0")
@@ -47,7 +55,7 @@ def read() -> Billboard:
             moviesList = s.find_all('div', class_="item_resa")
 
             for movie in moviesList:
-                
+
                 # Extract film information
                 film_data = movie.find('div', class_='j_w').get('data-movie')
                 film_info = json.loads(film_data)
@@ -57,7 +65,8 @@ def read() -> Billboard:
                 actors = film_info['actors']
 
                 # Extract cinema and time information
-                cinema_data = movie.find('div', class_='j_w').get('data-theater')
+                cinema_data = movie.find(
+                    'div', class_='j_w').get('data-theater')
                 cinema_info = json.loads(cinema_data)
                 cinema_name = cinema_info['name']
                 cinema_address = cinema_info['city']
@@ -72,20 +81,22 @@ def read() -> Billboard:
                 cinema = Cinema(cinema_name, cinema_address)
 
                 # Create Projection object
-                projection = Projection(film, cinema, (times[0].split(':')[0], times[0].split(':')[1]), language='')
+                projection = Projection(film, cinema, (times[0].split(':')[0],
+                                        times[0].split(':')[1]), language='')
 
                 # Add objects to the respective lists
                 if film not in lfilms:
                     lfilms.append(film)
 
-                if cinema.address == 'Barcelona':
-                    lcinemas.append(cinema)
+                lcinemas.append(cinema)
 
-                if projection.cinema.address == 'Barcelona':
-                    lprojections.append(projection)
+                lprojections.append(projection)
 
     # Create Billboard object
-    billboard = Billboard(films=lfilms, cinemas=lcinemas, projections=lprojections)
+    billboard = Billboard(
+        films=lfilms,
+        cinemas=lcinemas,
+        projections=lprojections)
 
     return billboard
 
@@ -93,17 +104,18 @@ def read() -> Billboard:
 def search_by_title(name: str, B: Billboard) -> list[Projection]:
     """ Given a name, returns a list of the projections
         that include this name in the title of the film"""
-    
     list_films: list[Projection] = list()
     for f in B.projections:
         if f.film.title == name:
             list_films.append(f)
-    
+
     return list_films
+
 
 def main() -> None:
     billboard = read()
     print(len(search_by_title('La Sirenita', billboard)))
+
 
 if __name__ == '__main__':
     main()
