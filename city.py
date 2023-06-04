12864657bb2bddeeb9003ed177de73e2a9aa42b3
CristinaTeixido <133266907+CristinaTeixido@users.noreply.github.com>
2023-06-04 19:57:30 +0200

    COMENTAT FALTA PEP PLOT PATH city.py

--- city.py
+++ city.py
@@ -16,6 +16,8 @@ Path: TypeAlias = list[int]         # list of nodes
 
 
 def get_osmnx_graph() -> OsmnxGraph:
+    """Creates the graph of Barcelona streets"""
+
     graph = ox.graph_from_place("Barcelona, Spain", network_type='all', truncate_by_edge=True)
     graph.graph['crs'] = 'EPSG:32631' #we determine coordinate's system 
     return graph
@@ -23,34 +25,37 @@ def get_osmnx_graph() -> OsmnxGraph:
 
 def get_simplified_graph(g: OsmnxGraph) -> nx.Graph:
     """Converts g to a networkx.graph"""
-    epsg_code = 'EPSG:32631'
 
+    epsg_code = 'EPSG:32631'
     graph_simplified = nx.Graph()
-    
     graph_simplified.graph['crs'] = epsg_code
 
-    for node, data in g.nodes(data=True):
+    # From nodes information we only take the coordinate
+    for node, data in g.nodes(data=True): 
         x = data['x']
         y = data['y']
-
         new_node_data = {'x': x, 'y': y}
-
         graph_simplified.add_node(node, **new_node_data)
 
+    #From the edges we only take the length and geometry information needed in further functions
     attributes = ["length", "geometry"]
 
-    for u, v, data in g.edges(data=True):
+    for u, v, data in g.edges(data=True): 
         selected_attributes = {key: data[key] for key in attributes if key in data}
-        graph_simplified.add_edge(u, v, **selected_attributes )
+        graph_simplified.add_edge(u, v, **selected_attributes)
+        length = graph_simplified.edges[u, v]['length']
+        graph_simplified.edges[u, v]['time'] = 0.15 * length
 
     return graph_simplified
 
 
 def save_osmnx_graph(g: OsmnxGraph, file_name: str) -> None:
     """Saves g in a file named filename"""
+
     with open(file_name, 'wb') as file:
         pickle.dump(g, file)
 
+    # Controls if the file_name given exists
     if os.path.exists(file_name):
         print("The file {} exists".format(file_name))
     
@@ -60,6 +65,7 @@ def save_osmnx_graph(g: OsmnxGraph, file_name: str) -> None:
 
 def load_osmnx_graph(file_name: str) -> OsmnxGraph:
     """Returns the graph saved in file_name"""
+
     if os.path.exists(file_name):
         with open(file_name, 'rb') as file:
             loaded_graph = pickle.load(file)
@@ -68,9 +74,9 @@ def load_osmnx_graph(file_name: str) -> OsmnxGraph:
     return None
 
 
-def plot_city(g:nx.Graph, filename: str) -> None:
-    """Saves g in a file named filename as an image with a map of the city as backgroung"""
-    map = staticmap.StaticMap(800,800)
+def plot_city(g:nx.Graph, map: staticmap.StaticMap) -> None:
+    """Draws all edges and nodes in the staticmap"""
+
     #We go through all nodes to draw the crosses
     for _, data in g.nodes(data=True): 
         coordinate = (data['x'], data['y'])
@@ -82,18 +88,19 @@ def plot_city(g:nx.Graph, filename: str) -> None:
         coord_destination = (g.nodes[destination]['x'], g.nodes[destination]['y'])
         map.add_line(staticmap.Line([coord_source, coord_destination], 'blue', 1))
 
-    image = map.render() #We save the image in a file named 'filename'
-    image.save(filename)
 
 
 def build_city_graph(g: OsmnxGraph, g1: nx.Graph, g2: BusesGraph) -> CityGraph:
     """Returns the graph of composing g1 and g2"""
+
+    # We add all stops of g2 to the simplified graph (g1)
     g1.add_nodes_from(g2.nodes(data=True))
     g1.add_edges_from(g2.edges(data=True))
 
+    # We link each stop node to nearest cross node (with length 0)
     for node, atributtes in g2.nodes(data=True):
         nearest_node = ox.distance.nearest_nodes(g, atributtes['coordinate'][0], atributtes['coordinate'][1])
-        g1.add_edge(node, nearest_node, length= 0.0)
+        g1.add_edge(node, nearest_node, length= 0.0, time=0.0)
 
     return g1
 
@@ -104,27 +111,33 @@ def show(g: CityGraph) -> None:
     plt.show()
 
 
-#def plot_city_buses(g: CityGraph, filename: str) -> None:
-#   """Saves city and buses in a file named filename as an image with a map of the city as backgroung"""
+def plotC(g1: buses.BusesGraph, g2: nx.Graph, filename: str) -> None:
+    """Plots the CityGraph using g1 and g2"""
+    map = staticmap.StaticMap(800, 800)
+    buses.plot_buses(g1, map)
+    plot_city(g2, map)
+    image = map.render()  # We save the image in a file named filename
+    image.save(filename)
 
 
 def find_path(ox_g: OsmnxGraph, g: CityGraph, src: Coord, dst: Coord) -> Path:
-    """Finds the shorter path to go from src to dst"""
+    """Finds the shorter path to go from src to dst using time parameter"""
+
     start_node = ox.distance.nearest_nodes(ox_g, src[0], src[1])
     end_node = ox.distance.nearest_nodes(ox_g, dst[0], dst[1])
 
-    path: Path = nx.shortest_path(g, source=start_node, target=end_node, weight= 'lenght')
+    path: Path = nx.shortest_path(g, source=start_node, target=end_node, weight= 'time')
 
-    print(path)
     return path
 
 
 def calculate_distance_path(g: CityGraph, path: Path) -> float:
     """Return the distance of path"""
-    distance = 0.0
 
+    distance = 0.0
     node = 1
-    while node < len(path):
+    # Go through all nodes in path
+    while node < len(path): 
         distance += g.edges[path[node - 1], path[node]]['length']
         node += 1
     
@@ -133,6 +146,7 @@ def calculate_distance_path(g: CityGraph, path: Path) -> float:
 
 def plot_path(g: CityGraph, p: Path, filename: str) -> None:
     """Saves an image named 'filename' with the plot of the path"""
+
     map = staticmap.StaticMap(800,800)
     index = 1
     while index < len(p):
@@ -155,21 +169,10 @@ def plot_interactive(filename:str)-> None:
     """Displays the file 'filename'"""
     try:
         plt.imshow(mpimg.imread(filename))
-        plt.axis('off')
+        plt.axis('off') # Removes the axis 
         plt.show()
 
     except FileNotFoundError:
         print("Arxiu no trobat")
     except IOError:
         print("Error durant la lectura de l'arxiu.")
-
-def main()-> None:
-    g = get_osmnx_graph()
-    G = get_simplified_graph(g)
-
-    city = build_city_graph(g, G, buses.define_nodes())
-
-    find_path(g, city, 2.1497259, 41.4067781)
-
-if __name__ == '__main__':
-    main()
